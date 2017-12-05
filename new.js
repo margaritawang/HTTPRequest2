60f8f3a029f0838843f23ad8992d37483da4cfef
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-05 00:12:27 +0000

    first commit

---
+++ new.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var fs = require('fs');
+
+request('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function(err) {
+    throw err;
+  })
+  .on('response', function(response) {
+    console.log(response.statusMessage, response.headers['content-type'])
+  })
+  .on('end', function(end) {
+  console.log('Download complete.');
+  })
+  .pipe(fs.createWriteStream('./future.jpg'))
+
+console.log('Downloading image...');
